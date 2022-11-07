e36adca353fe356d0be0955a93eff8ffa9e3a360
emanrdesu <eperezpozo@knights.ucf.edu>
2022-11-06 19:05:48 -0500

    Update States.java
    Deleted newlines.

--- java.exercises/src/test/java/States.java
+++ java.exercises/src/test/java/States.java
@@ -1,6 +1,5 @@
 
 public class States {
-
 	public static final State Texas = new State("Texas", "TX");
 	public static final State Louisiana = new State("Louisiana", "LA");
 	public static final State Florida = new State("Florida", "FL");
