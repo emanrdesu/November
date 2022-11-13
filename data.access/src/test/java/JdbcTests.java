ea72e34287f62c329821593b64e2810d5be163ce
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2022-11-13 11:15:01 -0500

    Fix Indentation

--- data.access/src/test/java/JdbcTests.java
+++ data.access/src/test/java/JdbcTests.java
@@ -24,31 +24,31 @@ public class JdbcTests {
         setup();
     }
 
+    @Test
+    public void canExecuteSingleColumn() {
+        var query = "select city from city order by city desc limit 10";
+        accessor.ExecuteSingleColumn(query);
+    }
+	
 	@Test
-	public void canExecuteSingleColumn() {
-	    var query = "select city from city order by city desc limit 10";
-	    accessor.ExecuteSingleColumn(query);
+    public void canExecuteCell() {
+        var query = "select max(amount) from payment";
+        accessor.ExecuteSingleCell(query).equals("11.99"); // true
 	}
 	
 	@Test
-	public void canExecuteCell() {
-	    var query = "select max(amount) from payment";
-	    accessor.ExecuteSingleCell(query).equals("11.99"); // true
-	}
-	
-	@Test
-	public void canExecuteView() {
-	    var query = "select * from film_list where actors like '%bob fawcett%'";
-	    accessor.Execute(query);
-	}
+    public void canExecuteView() {
+        var query = "select * from film_list where actors like '%bob fawcett%'";
+        accessor.Execute(query);
+    }
 
-	@Test
-	public void canExecuteStoredProcedure() {
-	    var query = "call film_in_stock " + 
-	                "((select film_id from film " +
-	                  "where title like '%alien center%'), " +
-	                  "2, @count)";
+    @Test
+    public void canExecuteStoredProcedure() {
+        var query = "call film_in_stock " + 
+                    "((select film_id from film " +
+                      "where title like '%alien center%'), " +
+                     "2, @count)";
 
-	    accessor.Execute(query);
+        accessor.Execute(query);
 	}
 }
