9f4c4f20608129561d81b1f594674849f5129d21
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2022-11-16 20:10:58 -0500

    Change tests to be in arrange, act, assert form.

--- data.access/src/test/java/JdbcTests.java
+++ data.access/src/test/java/JdbcTests.java
@@ -1,7 +1,8 @@
 import java.sql.*;
+import data.access.*;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import data.access.*;
+import org.testng.Assert;
 
 public class JdbcTests {
     private DatabaseAccessor accessor; 
@@ -20,19 +21,75 @@ public class JdbcTests {
     @Test
     public void canExecuteSingleColumn() {
         var query = "select city from city order by city desc limit 10";
-        accessor.ExecuteSingleColumn(query);
+        
+        String[] actual = accessor.ExecuteSingleColumn(query);
+        
+        String[] expected = {
+                "Ziguinchor",
+                "Zhoushan",
+                "Zhezqazghan",
+                "Zeleznogorsk",
+                "Zaria",
+                "Zapopan",
+                "Zaoyang",
+                "Zanzibar",
+                "Zalantun",
+                "Yuzhou"
+        };
+        
+        String errorMessage = "City should be returned.";
+        
+        for (int i = 0; i < actual.length; i++)
+            Assert.assertEquals(actual[i], expected[i], errorMessage);
     }
 	
     @Test
     public void canExecuteCell() {
         var query = "select max(amount) from payment";
-        accessor.ExecuteSingleCell(query).equals("11.99"); // true
+        var actual = accessor.ExecuteSingleCell(query); 
+        var expected = "11.99";
+        var errorMessage = "Number should be returned.";
+        Assert.assertEquals(actual, expected, errorMessage);
     }
 	
     @Test
     public void canExecuteView() {
-        var query = "select * from film_list where actors like '%bob fawcett%'";
-        accessor.Execute(query);
+        var query = "select title from film_list where actors like '%bob fawcett%'";
+        
+        DataRow[] actual = accessor.Execute(query);
+        
+        String[] expected = {
+                "ACE GOLDFINGER",
+                "ADAPTATION HOLES",
+                "CHINATOWN GLADIATOR",
+                "CIRCUS YOUTH",
+                "CONTROL ANTHEM",
+                "DARES PLUTO",
+                "DARN FORRESTER",
+                "DAZED PUNK",
+                "DYNAMITE TARZAN",
+                "HATE HANDICAP",
+                "HOMICIDE PEACH",
+                "JACKET FRISCO",
+                "JUMANJI BLADE",
+                "LAWLESS VISION",
+                "LEATHERNECKS DWARFS",
+                "OSCAR GOLD",
+                "PELICAN COMFORTS",
+                "PERSONAL LADYBUGS",
+                "RAGING AIRPLANE",
+                "RUN PACIFIC",
+                "RUNNER MADIGAN",
+                "SADDLE ANTITRUST",
+                "SCORPION APOLLO",
+                "SHAWSHANK BUBBLE",
+                "TAXI KICK"
+        };
+
+        var errorMessage = "Title should be returned.";
+        
+        for (int i = 0; i < actual.length; i++)
+            Assert.assertEquals(actual[i].GetColumns()[0], expected[i], errorMessage);
     }
 
     @Test
@@ -41,7 +98,12 @@ public class JdbcTests {
                     "((select film_id from film " +
                       "where title like '%alien center%'), " +
                      "2, @count)";
-
-        accessor.Execute(query);
+        
+        var actual = accessor.ExecuteSingleColumn(query);
+        String[] expected = { "73", "74", "75", "76" };
+        var errorMessage = "Inventory ID should be returned.";
+        
+        for (int i = 0; i < actual.length; i++)
+            Assert.assertEquals(actual[i], expected[i], errorMessage);        
     }
 }
