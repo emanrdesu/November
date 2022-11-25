118d87a4474cbcad139a67e9644b23e87162187b
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2022-11-25 05:18:42 -0500

    Configure Timeout and Maximize driver options

--- Ampeg/src/test/java/Sanity.java
+++ Ampeg/src/test/java/Sanity.java
@@ -3,18 +3,25 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import ampeg_auto.DriverManager;
 import ampeg_auto.DriverManagerFactory;
 
 public class Sanity {
     private DriverManager driverManager;
+    private WebDriver driver;
 
     @BeforeTest
     public void setup() {
         String browserType = "chrome";
+
         driverManager = DriverManagerFactory.getManager(browserType);
         driverManager.createDriver();
+        driver = driverManager.getDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
     @AfterTest
@@ -25,8 +32,6 @@ public class Sanity {
     @Test
     public void canLaunchWebDriver() {
       var url = "https://ampeg.com/index.html";
-      WebDriver driver = driverManager.getDriver();
-      
       driver.navigate().to(url);
       Assert.assertEquals(driver.getCurrentUrl(), url, "Ampeg URL expected");
   }
