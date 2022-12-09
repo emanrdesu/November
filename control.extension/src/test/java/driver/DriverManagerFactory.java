180a8437813520448fa7c8f95c4d9474ab5f5781
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2022-12-08 23:42:01 -0500

    Change getManager so that it throws if browser is not supported

--- control.extension/src/test/java/driver/DriverManagerFactory.java
+++ control.extension/src/test/java/driver/DriverManagerFactory.java
@@ -4,7 +4,10 @@ public class DriverManagerFactory {
     public static DriverManager getManager(String browser) {
         if (browser.equals("chrome"))
             return new ChromeDriverManager();
-        else
+
+        if (browser.equals("edge"))
             return new EdgeDriverManager();
+        
+        throw new IllegalArgumentException("browser: " + browser + " is not supported.");
     }
 }
