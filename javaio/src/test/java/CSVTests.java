19a11757b9b960119da8387f5f6274d9031be108
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2022-12-11 22:49:39 -0500

    Add Unit Tests, Override and Add Bass.equals method(s)

--- javaio/src/test/java/CSVTests.java
+++ javaio/src/test/java/CSVTests.java
@@ -1,7 +1,71 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import javaio.Bass;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class CSVTests {
+    private List<Bass> basses;
+    
+    @BeforeMethod
+    public void setup() throws FileNotFoundException {
+        String bassesFile = "../java.io/src/test/resources/basses.csv";
+        FileReader bassesReader = new FileReader(bassesFile);
+        this.basses = new CsvToBeanBuilder<Bass>(bassesReader)
+                           .withType(Bass.class).build().parse();
+    }
+
+    private Map<String, String> getBassesHashMap() {
+        Map<String, String> result = new HashMap<String, String>();
+
+        for(Bass b : this.basses)
+            result.put(b.getMake(), b.getModel());
+
+        return result;
+    }
+
     @Test
-    public void f() {
+    public void canGetMakesandModels() {
+        var expected = new HashMap<>(){{
+            put("Warwick","Streamer");
+            put("Fender","Jazz");
+            put("Yamaha","BB500");
+        }};
+
+        var actual = getBassesHashMap();
+
+        var errorMessage1 = "Same hashMap keys expected";
+        Assert.assertEquals(actual.keySet(), expected.keySet(), errorMessage1);
+        
+        var errorMessage2 = "Same key,value bass expected";
+        for(String key : actual.keySet())
+            Assert.assertEquals(actual.get(key), expected.get(key), errorMessage2);
+    }
+
+    @Test
+    public void canPopulateBass() {
+        Bass[] expected = {
+           new Bass("Warwick", "Corvette", "5"),
+           new Bass("Warwick", "Thumb", "5"),
+           new Bass("Warwick", "Streamer", "5"),
+           new Bass("Fender", "Precision", "4"),
+           new Bass("Fender", "Jazz", "4"),
+           new Bass("Yamaha", "BB500", "5")
+        };
+
+        Bass[] actual = this.basses.toArray(new Bass[0]);
+
+        Assert.assertEquals(actual.length, expected.length, "Same amount of bass expected");
+        for(int i = 0; i < actual.length; i++)
+            Assert.assertEquals(actual[i], expected[i], "Same bass expected");
     }
 }
--- javaio/src/test/java/javaio/Bass.java
+++ javaio/src/test/java/javaio/Bass.java
@@ -13,10 +13,26 @@ public class Bass {
     @CsvBindByName
     private String StringCount;
 
+    public Bass() { super(); }
+
     public Bass(String Make, String Model, String Count) {
         this.Make = Make;
         this.Model = Model;
         this.StringCount = Count;
+    }
+
+    @Override
+    public boolean equals(Object bass) {
+        if (bass instanceof Bass)
+            return this.equals((Bass) bass);
+        else
+            return super.equals(bass);
+    }
+
+    public boolean equals(Bass bass) {
+        return getMake().equals(bass.getMake()) &&
+               getModel().equals(bass.getModel()) &&
+               getStringCount().equals(bass.getStringCount());
     }
 
     public String getMake() {
