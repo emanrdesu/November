09b2f4396c471499df855f9bd19a6e7b8672b06c
Ernesto Perez Pozo <eperezpozo@knights.ucf.edu>
2023-01-05 13:46:05 -0500

    Add Test #1

--- reqres.api/src/test/java/APITests.java
+++ reqres.api/src/test/java/APITests.java
@@ -5,14 +5,28 @@ import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.matcher.RestAssuredMatchers.*;
-import org.hamcrest.Matchers.*;
+import org.hamcrest.Matchers;
 
 public class APITests {
+    
+    private String index = "https://reqres.in";  
+    
     @Test
     public void canRun() {
         RestAssured.when().
-            get("https://reqres.in/").
+            get(index).
         then().
             statusCode(200);
+    }
+    
+    @Test
+    public void canGetEmailAddress() {
+        var expectedEmail = "janet.weaver@reqres.in";
+
+        RestAssured.when().
+            get(index + "/api/users/2").
+        then().
+            statusCode(200).
+            body("data.email", Matchers.equalTo(expectedEmail));
     }
 }
